9aa4d7a69d07a94ae0bc2874af3ad74fa2697f5e
halv00rsen <jorgen-halvorsen@hotmail.com>
2015-02-24 14:16:09 +0100

    Events kan bli lagt til

--- src/classes/Calendar.java
+++ src/classes/Calendar.java
@@ -5,12 +5,22 @@ import java.util.List;
 
 public class Calendar {
 	
-	private List<Event> events;
+	private final List<Event> events;
 	
 	public Calendar(){
 		events = new ArrayList<Event>();
 		//hent data fra database
 		
 		
+	}
+	
+	public void addEvent(Event event){
+		if (event != null)
+			events.add(event);
+	}
+	
+	public void removeEvent(Event event){
+		if (event != null)
+			events.remove(event);
 	}
 }
