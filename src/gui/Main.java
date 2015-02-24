ecc639539d2eefedecd94e7f712e6b2f70e5ae8f
Magnus <Magnus@78.91.6.210>
2015-02-24 13:20:46 +0100

    la til height og width

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,screenHeight,screenWidth);
+			Scene scene = new Scene(root,SCREENHEIGHT,SCREENHEIGHT);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
