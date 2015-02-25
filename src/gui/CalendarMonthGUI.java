72bd0ee84086070e5c18982cf9a7d52ee5ab6b4f
Magnus <Magnus@78.91.6.210>
2015-02-25 13:37:18 +0100

    Lagt til kalender

--- src/gui/CalendarMonthGUI.java
+++ src/gui/CalendarMonthGUI.java
@@ -1,17 +1,24 @@
 package gui;
 
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Border;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.TriangleMesh;
 
 import java.time.Month;
 import java.time.Year;
@@ -19,52 +26,96 @@ import java.time.ZonedDateTime;
 
 public class CalendarMonthGUI extends Component{
 	ZonedDateTime date;
+	static int defaulCalHeight = 300;
+	static int defaulCalWidth = 300;
+	CalendarMonthBase mainCalendar;
+	CalendarMonthBase nextCalendar;
+	CalendarMonthBase lastCalendar;
 	Month month; 
 	int daysInMonth;
 	GridPane grid; 
 	String brand = "Kalender";
 	Label l;
 	VBox box;
+	HBox hbox;
+	HBox animeBox;
+	StackPane clip;
 	Pane dayBox;
-	public CalendarMonthGUI(Pane parent, ZonedDateTime date) {
+	Slider sliderLeft;
+	Slider sliderRight;
+	public CalendarMonthGUI(Pane parent) {
 		super(parent);
-		this.date = date;
-		this.month = date.getMonth();
+		updateDate(ZonedDateTime.now());
 		
-		if( Year.isLeap(date.getYear())){
+		
+		this.setPrefWidth(100);
+		this.setPrefHeight(500);
+
+		this.lastCalendar = new CalendarMonthBase(this.date.plusMonths(-1));
+		this.mainCalendar = new CalendarMonthBase(date);
+		this.nextCalendar = new CalendarMonthBase(this.date.plusMonths(1));
+		
+		
+		
+		animeBox = new HBox();
+		animeBox.setAlignment(Pos.BASELINE_CENTER);
+		//animeBox.getChildren().add(lastCalendar);
+		animeBox.getChildren().add(mainCalendar);
+		//animeBox.getChildren().add(nextCalendar);
+
+//		clip.getChildren().add(clippingShape);
+//		clip.getChildren().get(0).setClip(clippingShape);
+		
+		sliderLeft = new Slider(true);
+		sliderRight = new Slider(false);
+		hbox = new HBox();
+		hbox.getChildren().add(sliderLeft);
+		hbox.getChildren().add(clip);
+		hbox.getChildren().add(sliderRight);
+		this.getChildren().add(hbox);
+		sliderRight.setOnMouseClicked(e -> nextSlide(e));
+		sliderLeft.setOnMouseClicked(e -> nextSlide(e));
+		
+	}
+	private void updateDate(ZonedDateTime d) {
+		this.date = d;
+		this.month = d.getMonth();
+		
+		if( Year.isLeap(d.getYear())){
 			daysInMonth = month.maxLength();
 		}else{
 			daysInMonth = month.minLength();
 		}
-		this.setPrefWidth(500);
-		this.setPrefHeight(500);
-		grid = new GridPane();
-		
-		BorderStroke gridBorderStroke = new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT);
-		Border gridBorder = new Border(gridBorderStroke);
-		grid.setBorder(gridBorder);
-		int x = 0;
-		int y = 0;
-		for (int i = 0; i < daysInMonth; i++) {
-			CalendarDayBox thisDay = new CalendarDayBox();
-			y += 1;
-			if(i % 7 == 0){
-				x += 1;
-				y = 0;
-			}
-			grid.add(thisDay,y,x);
-		}
-		
-		l = new Label();
-		l.setText(brand);
-		box = new VBox();
-		this.getChildren().add(box);
-		box.getChildren().add(l);
-		box.getChildren().add(grid);
-		grid.setGridLinesVisible(true);
-		
 	}
+	private void nextSlide(MouseEvent e) {
+		int offset = 0;
+		clip.getChildren().add(animeBox);
+		Rectangle clippingShape = new Rectangle();
+		clip = new StackPane();
+		if(e.getSource().equals(sliderRight)){
+			offset = 1;
+			mainCalendar.setVisible(false);
+			mainCalendar = nextCalendar;
+			animeBox.getChildren().set(0,mainCalendar);
+			mainCalendar.setVisible(true);
+
+			updateDate( mainCalendar.getZonedDateTime());
+		}else if (e.getSource().equals(sliderLeft)){
+			offset = -1;
+			mainCalendar.setVisible(false);
+			mainCalendar = lastCalendar;
+			animeBox.getChildren().set(0,mainCalendar);
+
+			mainCalendar.setVisible(true);
+			updateDate(mainCalendar.getZonedDateTime());
+		}
+		this.lastCalendar = new CalendarMonthBase(this.date.plusMonths(-1));
+		this.mainCalendar = new CalendarMonthBase(date);
+		this.nextCalendar = new CalendarMonthBase(this.date.plusMonths(1));
 		
+
+	}
 	
+
 	
 }
