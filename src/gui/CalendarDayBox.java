859a684dd7cc08498b233e827f81025073aa4a70
Magnus <Magnus@78.91.6.210>
2015-02-25 13:37:04 +0100

    Boksen representere dagene i kalenderen

--- src/gui/CalendarDayBox.java
+++ src/gui/CalendarDayBox.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.time.DayOfWeek;
 import java.time.ZonedDateTime;
 
 import javafx.scene.control.Label;
@@ -12,18 +13,31 @@ public class CalendarDayBox extends Pane{
 	int dayOfMonth;
 	int month;
 	Label day;
-	public CalendarDayBox(){//ZonedDateTime date,ArrayList<Event> events){
-//		this.dayOfMonth = date.getDayOfMonth();
-//		this.month = date.getMonthValue();
-		this.dayOfMonth = 31;
-		this.month = 12;
-		this.setPrefWidth(80);
-		this.setPrefHeight(80);
-		this.setStyle("-fx-background-color: #AAAAAA");
+	public int calHeight = 100;
+	public int calWidth = 100;
+	public CalendarDayBox(ZonedDateTime date,ArrayList<Event> events){
+		this.dayOfMonth = date.getDayOfMonth();
+		this.month = date.getMonthValue();
+//		this.dayOfMonth = 31;
+		this.setPrefWidth(calWidth);
+		this.setPrefHeight(calHeight);
+		//Add red color if sunday
+		if(date.getDayOfWeek() == DayOfWeek.SUNDAY){
+			this.setStyle("-fx-background-color: #FAA");
+		}else{
+			this.setStyle("-fx-background-color: #FFF");
+		}
 		day = new Label();
 		day.setText("" + dayOfMonth);
 		this.getChildren().add(day);
 		
 		
+	}
+
+	public void setSize(int calHeight, int calWidth){
+		this.calHeight = calHeight;
+		this.calWidth = calWidth;
+		this.setPrefWidth(calWidth);
+		this.setPrefHeight(calHeight);
 	}
 }
