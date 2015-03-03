52576ed6de17ca71bdfd25a2e6e3866e738a2e1c
Magnus <Magnus@78.91.5.26>
2015-03-03 16:37:55 +0100

    windows

---
+++ src/windows/GroupScreen.java
@@ -0,0 +1,14 @@
+package windows;
+import components.*;
+import gui.*;
+import javafx.scene.layout.Pane;
+import gui.Window;
+public class GroupScreen extends Window{
+
+	@Override
+	public void init(Pane root) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
