e9abbdeb7f6e0ed22aa23a98366e27fe45d54c2d
Simen Wilberg <simendavanger@gmail.com>
2015-03-05 16:24:03 +0100

    createUser

--- src/windows/NewUserWindow.java
+++ src/windows/NewUserWindow.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -44,8 +45,8 @@ public class NewUserWindow extends Window{
 	TextField emailTextField;
 	TextField phoneTextField;
 	TextField usernameTextField;
-	TextField passwordTextField;
-	TextField rePasswordTextField;
+	PasswordField passwordTextField;
+	PasswordField rePasswordTextField;
 	
 	TextFlow terms;
 	CheckBox acceptCheckBox;
@@ -148,7 +149,7 @@ public class NewUserWindow extends Window{
 		    }
 		});
 		
-		passwordTextField = new TextField();
+		passwordTextField = new PasswordField();
 		passwordTextField.setPromptText("******");
 		passwordTextField.focusedProperty().addListener(new ChangeListener<Boolean>()
 				{
@@ -163,7 +164,7 @@ public class NewUserWindow extends Window{
 		    }
 		});
 		
-		rePasswordTextField = new TextField();
+		rePasswordTextField = new PasswordField();
 		rePasswordTextField.setPromptText("******");
 		rePasswordTextField.focusedProperty().addListener(new ChangeListener<Boolean>()
 				{
