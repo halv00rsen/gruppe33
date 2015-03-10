787e16ff5f6d4ff9b86d2885da73adeb57d9756d
halv00rsen <jorgen-halvorsen@hotmail.com>
2015-03-10 14:17:06 +0100

    meldingsboksen starter nå utenfor bildet, og kommer inn når man trykker på en knapp

--- src/gui/MessageScreen.java
+++ src/gui/MessageScreen.java
@@ -1,34 +1,52 @@
 package gui;
 
 
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
+import javafx.application.Platform;
 import javafx.scene.Group;
+import javafx.scene.control.TextArea;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 public class MessageScreen extends Group{
 
 	private final int height = 100, width = 200;
 	private final Rectangle rec;
+	private final Text message;
 	
 	public MessageScreen(){
 		rec = new Rectangle(width, height);
 		rec.setFill(Color.BEIGE);
 		rec.setStrokeWidth(3);
 		rec.setStroke(Color.BLACK);
+		message = new Text("heisanndu");
+		message.setX(20);
+		message.setY(40);
 //		rec.setStyle("-fx-border: 3; -fx-border-color: #FF9933; -fx-background-color: #FFB870");
 		Text header = new Text("Melding!");
-		header.setLayoutY(10);
-		getChildren().addAll(rec, header);
-		setLayoutX(150);
-		setLayoutY(80);
+		header.setX(20);
+		header.setY(20);
+		getChildren().addAll(rec, header, message);
+		setLayoutX(280);
+		setLayoutY(- 1 * height - 5);
 	}
 	
 	public void show(String message){
-		
+		TranslateTransition tt = new TranslateTransition(Duration.seconds(1), this);
+		tt.setFromY(- 1 * height);
+		tt.setToY(40 + height);
+	    tt.setCycleCount(1);
+	    tt.play();
 	}
 	
 	public void hide(){
-		
+		TranslateTransition tt = new TranslateTransition(Duration.seconds(1), this);
+		tt.setFromY(40 + height);
+		tt.setToY(- 1 * height);
+		tt.setCycleCount(1);
+		tt.play();
 	}
 }
