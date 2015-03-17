99aac6d1706e8b52d38d13683635f069c97b5299
Simen Wilberg <simendavanger@gmail.com>
2015-03-17 14:11:22 +0100

    CMSQL

--- src/components/LoginGUI.java
+++ src/components/LoginGUI.java
@@ -164,7 +164,8 @@ public class LoginGUI extends Component{
 
 			@Override
 			public void handle(ActionEvent arg0) {
-				
+		
+				System.out.println(ConnectionMySQL.getGroups("Wilberg93"));
 				validateEmail();
 				
 			}
